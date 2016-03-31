317bdc91a5851fa6f169d68dd54c5c6243a44a63
Vadym Yatsyuk <vasp3d@gmail.com>
2016-03-31 12:59:38 +0200

    minor, removed comments

--- main.js
+++ main.js
@@ -33,9 +33,6 @@ function createWindow () {
   mainWindow.loadURL('file://' + __dirname + '/main/main.html');
 
   mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     mainWindow = null;
   });
 
@@ -65,22 +62,15 @@ function createWindow () {
   });
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
 app.on('ready', createWindow);
 
-// Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
 app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
     createWindow();
   }
